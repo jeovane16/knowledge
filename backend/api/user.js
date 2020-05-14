23eb84c14edb78ba4f6d9bb1f3cfe2e8a1e84818
jeovane16 <jeovanearaujo16@gmail.com>
2020-05-14 16:36:49 -0300

    Criação de tokens na api

--- backend/api/user.js
+++ backend/api/user.js
@@ -59,12 +59,26 @@ module.exports = app => {
     const getById = (req, res) => {
         app.db('users')
             .select('id', 'name', 'email', 'admin')
-            .where({ id: req.params.id })
+            .where({ id: req.params.id})
             .whereNull('deletedAt')
             .first()
             .then(user => res.json(user))
             .catch(err => res.status(500).send(err))
     }
 
-    return { save, get, getById }
+    const remove = async (req, res) => {
+        try {
+            const articles = await app.db('articles').where({ userId: req.params.id });
+            notExistsOrError(articles, 'Usuário possui artigos.');
+
+            const rowsUpdated = await app.db('users').update({deletedAt: new Date()}).where({ id: req.params.id });
+            existsOrError(rowsUpdated, 'Usuário não foi encontrado.');
+
+            res.status(204).send();
+        } catch(msg) {
+            res.status(400).send(msg);
+        }
+    }
+
+    return { save, get, getById, remove }
 }
--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,8 +1,42 @@
 module.exports = app => {
+
+    app.post('/signup', app.api.user.save)
+    app.post('/signin', app.api.auth.signin)
+    app.post('/validateToken', app.api.auth.validateToken)
+
     app.route('/users')
         .post(app.api.user.save)
         .get(app.api.user.get)
 
-    app.route('/users:id')
+    app.route('/users/:id')
         .put(app.api.user.save)
+        .get(app.api.user.getById)
+
+    app.route('/categories')
+        .get(app.api.category.get)
+        .post(app.api.category.save)
+
+    app.route('/categories/tree')
+        .all(app.config.passport.authenticate())
+        .get(app.api.category.getTree)
+
+    app.route('/categories/:id')
+        .get(app.api.category.getById)
+        .put(app.api.category.save)
+        .delete(app.api.category.remove)
+
+    app.route('/articles')
+        .all(app.config.passport.authenticate())
+        .get(admin(app.api.article.get))
+        .post(admin(app.api.article.save))
+
+    app.route('/articles/:id')
+        .all(app.config.passport.authenticate())
+        .get(app.api.article.getById)
+        .put(admin(app.api.article.save))
+        .delete(admin(app.api.article.remove))
+
+    app.route('/categories/:id/articles')
+        .all(app.config.passport.authenticate())
+        .get(app.api.article.getByCategory)
 }
